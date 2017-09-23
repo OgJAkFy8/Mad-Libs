80b97d1bf39715e9a374aff98b4a3be31adbb918
Erik Arnesen <earnesen@ymail.com>
2017-09-23 07:59:20 -0400

    minor change
    Added a check for the case where there are NO blankWords.

--- Mad-Libs.py
+++ Mad-Libs.py
@@ -3,26 +3,29 @@ from collections import Counter
 import re
 import string
 
+# Open the file
+txtfile = open('.\campletter1.txt','r')
+Message = txtfile.read()
+txtfile.close()
+
 
 # Simple Replace
 actionWord = ["hit", "climb","run"]
 personWord = ["bar tender","baseball Ump","trash man"]
 thingWord = ["boat"]
 
-printMess = 'We are having the best time in %s camp.  The %s likes to %s a pinecone.'%(personWord[randint(0,len(personWord))],personWord[randint(0,len(personWord))],actionWord[randint(0,len(actionWord))])
-print(printMess)
+##printMess = 'We are having the best time in %s camp.  The %s likes to %s a pinecone.'%(personWord[randint(0,len(personWord))],personWord[randint(0,len(personWord))],actionWord[randint(0,len(actionWord))])
+##print(printMess)
 print(4 * '\n')
-
-
 
 
 # User input version
 # Start with the same text
-Message = 'We are having the best time in thingWord camp.  The personWord likes to actionWord a pinecone.'
+#Message = 'We are having the best time in thingWord camp.  The personWord likes to actionWord a pinecone.'
 printMess = Message.split()
 printMes2 = Message
 
-# Count the words in the text 
+# Count the words in the text
 cnt = Counter()
 for word in printMess:
     cnt[word] += 1
@@ -36,22 +39,29 @@ actionWord.clear()
 personWord.clear()
 thingWord.clear()
 
+print(blue)
+print(red)
+print(green)
+
 # Enter the words
-for i in range(0,green):
-	actionWord.insert(i,input("Enter action word: "))
-	o = actionWord[i]
-	printMes2 = printMes2.replace('actionWord', o,1)
+if green != None:
+    for i in range(0,green):
+        actionWord.insert(i,input("Enter action word: "))
+        o = actionWord[i]
+        printMes2 = printMes2.replace('actionWord', o,1)
 
-for i in range(0,blue):
-	thingWord.insert(i,input("Enter thing word: "))
-	o = thingWord[i]
-	printMes2 = printMes2.replace('thingWord', o,1)
+if blue != None:
+    for i in range(0,blue):
+        thingWord.insert(i,input("Enter thing word: "))
+        o = thingWord[i]
+        printMes2 = printMes2.replace('thingWord', o,1)
 
-for i in range(0,red):
-	personWord.insert(i,input("Enter person's job title: "))
-	o = personWord[i]
-	printMes2 = printMes2.replace('personWord', o,1)
-	
+if red  != None:
+    for i in range(0,red):
+        personWord.insert(i,input("Enter person's job title: "))
+        o = personWord[i]
+        printMes2 = printMes2.replace('personWord', o,1)
+
 print(4 * '\n')
 
 # print the new message
