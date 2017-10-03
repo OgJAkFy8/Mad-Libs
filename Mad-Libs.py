a97eb977c24b222f588db28540f95201b4444fdd
OgJAkFy8 <OgJAkFy8@users.noreply.github.com>
2017-10-03 09:12:06 -0400

    Updated

--- Mad-Libs.py
+++ Mad-Libs.py
@@ -3,83 +3,57 @@ from collections import Counter
 import re, string, time, os
 
 
-# Define the lists
-actionWord = [""]
-personWord = [""]
-thingWord = [""]
+# Assignments
+key = set("$")
+i=1
 
 
 # Open the file
+print('\n')
 blankfiles = os.listdir('blank')
-i=1
 for b_file in blankfiles:
     #print(i+". "+b_file)
     print(str(i)+'. '+b_file)
     i=i+1
 txtfilenum =input("Enter number of the template: ")
 
-txtfile = open('blank/'+blankfiles[int(txtfilenum)],'r')
+txtfile = open('blank/'+blankfiles[int(txtfilenum)-1],'r')
 Message = txtfile.read()
 txtfile.close()
 
-print(4 * '\n')
+print(2 * '\n')
 
-# User input version
-printMess = Message.split()
-printMes2 = Message
+# Convert message to working variables
+lst_letter = Message.split()
 
 # File Name
-filename = printMess[0]+"-"+printMess[1]
-printMess.remove(printMess[0])
-printMess.remove(printMess[0])
+filename = lst_letter[0]+"-"+lst_letter[1]
+del lst_letter[0]
+del lst_letter[0]
 
-# Count the words in the text
-cnt = Counter()
-for word in printMess:
-    cnt[word] += 1
+str_letter = ' '.join(map(str, lst_letter))
 
-# Select the key words
-blue = cnt.get('thingWord')
-red = cnt.get('personWord')
-green = cnt.get('actionWord')
 
-actionWord.clear()
-personWord.clear()
-thingWord.clear()
+# Find the words to replace.
+# Have the user enter the words
+# Replace the words
+for word in lst_letter:
+    if key & set(word):
+        word_key=word.split("$")
+        word_replacement=(input("Enter a/an "+word_key[1]+": "))
+        str_letter = str_letter.replace(word, word_replacement,1)
 
-# print(blue)
-# print(red)
-# print(green)
-
-# Enter the words
-if green != None:
-    for i in range(0,green):
-        actionWord.insert(i,input("Enter action word: "))
-        o = actionWord[i]
-        printMes2 = printMes2.replace('actionWord', o,1)
-
-if blue != None:
-    for i in range(0,blue):
-        thingWord.insert(i,input("Enter thing word: "))
-        o = thingWord[i]
-        printMes2 = printMes2.replace('thingWord', o,1)
-
-if red  != None:
-    for i in range(0,red):
-        personWord.insert(i,input("Enter person's job title: "))
-        o = personWord[i]
-        printMes2 = printMes2.replace('personWord', o,1)
-
-#print(4 * '\n')
 
 # Create output file and write
 # File Name
 timestr = time.strftime("%d%M%S")
 #print (filename)
 f = open('finished/'+filename+'_'+timestr+'.txt', 'w')
-f.write(printMes2)
+f.write(str_letter)
 f.close()
 
 # print the new message
-print(printMes2)
+print('\n')
+print(str_letter)
+print('\n')
 print('finished/'+filename+'_'+timestr+'.txt')
