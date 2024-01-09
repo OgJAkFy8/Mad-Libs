6974d67fe4c32ff8fd3cfeb7f494e13c75f7b3a6
Erik JA <OgJAkFy8@users.noreply.github.com>
2024-01-09 14:12:35 -0600

    Update start_MadLibs.py

--- start_MadLibs.py
+++ start_MadLibs.py
@@ -3,33 +3,39 @@ from modules.madregexModule import modifyString
 import tkinter as tk
 from tkinter import messagebox
 
-# BEGIN
-# Variables
-stationaryPad = 'Blank-Letters'
-mailBox = 'Outputs'
 
-# Pop-up window for viewing the output
-def popup(message, title=None):
-    root = tk.Tk()
-    root.withdraw()
-    root.wm_attributes("-topmost", 1)
-    messagebox.showinfo(title, message, parent=root)
-    root.destroy()
+def main():
+    # BEGIN
+    # Variables
+    stationaryPad = 'Blank-Letters'
+    mailBox = 'Outputs'
 
-# PROCESS
-file_path, filename = Select_Letter(stationaryPad)
+    # Pop-up window for viewing the output
+    def popup(message, title=None):
+        root = tk.Tk()
+        root.withdraw()
+        root.wm_attributes("-topmost", 1)
+        messagebox.showinfo(title, message, parent=root)
+        root.destroy()
 
-#print(filename)
-#print(file_path)
-if(file_path != 'Blank-Letters/Keywords'):
-    BlankLetter, filename = Assemble_Letter(file_path, filename)
-    Letter = modifyString(BlankLetter)
-    MsgPath, Message = Write_Letter(mailBox, filename, Letter)
-    # END
-    popup(Message, MsgPath)
-else:
-    #print('else')
-    f = open(file_path, 'r')
-    t = f.read().split('\n')
-    for i in range(0, (len(t))):
-        print(t[i])
+    # PROCESS
+    file_path, filename = Select_Letter(stationaryPad)
+
+    #print(filename)
+    #print(file_path)
+    if(file_path != 'Blank-Letters/Keywords'):
+        BlankLetter, filename = Assemble_Letter(file_path, filename)
+        Letter = modifyString(BlankLetter)
+        MsgPath, Message = Write_Letter(mailBox, filename, Letter)
+        # END
+        popup(Message, MsgPath)
+    else:
+        #print('else')
+        f = open(file_path, 'r')
+        t = f.read().split('\n')
+        for i in range(0, (len(t))):
+            print(t[i])
+
+
+if __main__ == "__main__":
+    main()
