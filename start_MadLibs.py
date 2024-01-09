a20bc913ac58b3327b8e75a59cc477989522636b
Erik JA <OgJAkFy8@users.noreply.github.com>
2024-01-09 14:35:26 -0600

    revert changes

--- start_MadLibs.py
+++ start_MadLibs.py
@@ -4,38 +4,38 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-def main():
-    # BEGIN
-    # Variables
-    stationaryPad = 'Blank-Letters'
-    mailBox = 'Outputs'
+#def main():
+# BEGIN
+# Variables
+stationaryPad = 'Blank-Letters'
+mailBox = 'Outputs'
 
-    # Pop-up window for viewing the output
-    def popup(message, title=None):
-        root = tk.Tk()
-        root.withdraw()
-        root.wm_attributes("-topmost", 1)
-        messagebox.showinfo(title, message, parent=root)
-        root.destroy()
+# Pop-up window for viewing the output
+def popup(message, title=None):
+    root = tk.Tk()
+    root.withdraw()
+    root.wm_attributes("-topmost", 1)
+    messagebox.showinfo(title, message, parent=root)
+    root.destroy()
 
-    # PROCESS
-    file_path, filename = Select_Letter(stationaryPad)
+# PROCESS
+file_path, filename = Select_Letter(stationaryPad)
 
-    #print(filename)
-    #print(file_path)
-    if(file_path != 'Blank-Letters/Keywords'):
-        BlankLetter, filename = Assemble_Letter(file_path, filename)
-        Letter = modifyString(BlankLetter)
-        MsgPath, Message = Write_Letter(mailBox, filename, Letter)
-        # END
-        popup(Message, MsgPath)
-    else:
-        #print('else')
-        f = open(file_path, 'r')
-        t = f.read().split('\n')
-        for i in range(0, (len(t))):
-            print(t[i])
+#print(filename)
+#print(file_path)
+if(file_path != 'Blank-Letters/Keywords'):
+    BlankLetter, filename = Assemble_Letter(file_path, filename)
+    Letter = modifyString(BlankLetter)
+    MsgPath, Message = Write_Letter(mailBox, filename, Letter)
+    # END
+    popup(Message, MsgPath)
+else:
+    #print('else')
+    f = open(file_path, 'r')
+    t = f.read().split('\n')
+    for i in range(0, (len(t))):
+        print(t[i])
 
 
-if __main__ == "__main__":
-    main()
+#if __main__ == "__main__":
+ #   main()
